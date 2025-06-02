00ab91b9f4b304ea4939996f9515c10471386b04
YounesDjelloul <younesdjelloul14@gmail.com>
2025-06-03 01:46:10 +0800

    using zod and form hook to handle form state, validation and errors

--- app/registration.tsx
+++ app/registration.tsx
@@ -1,12 +1,39 @@
-import React, {useState} from 'react';
-import {Text, View} from "react-native";
+import React from 'react';
+import {Pressable, Text, View} from "react-native";
 import InputField from "@/components/base/InputField";
 import SafeContainer from "@/components/SafeContainer";
+import {z} from "zod";
+import {Controller, useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {Link} from "expo-router";
+
+interface State {
+    username: string
+    email: string
+    password: string
+}
+
+const registrationSchema = z.object({
+    username: z.string(),
+    email: z.string().email('Please enter a valid email'),
+    password: z.string().min(6, 'Password must be at least 6 characters'),
+});
+
+const initialValues: State = {
+    username: '',
+    email: '',
+    password: ''
+}
 
 const Registration = () => {
-    const [username, setUsername] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    const {control, handleSubmit, formState: {errors, isSubmitting}} = useForm({
+        resolver: zodResolver(registrationSchema),
+        defaultValues: initialValues,
+    });
+
+    const onSubmit = async (data: State) => {
+        console.log(data)
+    }
 
     return (
         <SafeContainer>
@@ -17,32 +44,61 @@ const Registration = () => {
                         Create your account to get started
                     </Text>
                 </View>
-                <InputField
-                    label="USER NAME"
-                    placeholder='Username'
-                    value={username}
-                    onChangeText={setUsername}
+                <Controller
+                    control={control}
+                    name="username"
+                    render={({field: {onChange, value}}) => (
+                        <InputField
+                            label="USER NAME"
+                            placeholder='Username'
+                            value={value}
+                            onChangeText={onChange}
+                            error={errors.username?.message}
+                        />
+                    )}
                 />
-                <InputField
-                    label="EMAIL ADDRESS"
-                    placeholder="you@example.com"
-                    value={email}
-                    onChangeText={setEmail}
+                <Controller
+                    control={control}
+                    name="email"
+                    render={({field: {onChange, value}}) => (
+                        <InputField
+                            label="EMAIL ADDRESS"
+                            placeholder='you@example.com'
+                            value={value}
+                            onChangeText={onChange}
+                            error={errors.email?.message}
+                        />
+                    )}
                 />
-                <InputField
-                    label="PASSWORD"
-                    placeholder="password"
-                    value={password}
-                    onChangeText={setPassword}
+                <Controller
+                    control={control}
+                    name="password"
+                    render={({field: {onChange, value}}) => (
+                        <InputField
+                            label="PASSWORD"
+                            placeholder="password"
+                            value={value}
+                            onChangeText={onChange}
+                            error={errors.password?.message}
+                            secureTextEntry
+                        />
+                    )}
                 />
-                <View className="bg-blue-500 py-3 mt-6 px-8 w-[95%] rounded-full">
+
+                <Pressable
+                    onPress={handleSubmit(onSubmit)}
+                    disabled={isSubmitting}
+                    className="bg-blue-500 py-3 mt-6 px-8 w-[95%] rounded-full"
+                >
                     <Text className="font-button text-lg text-white text-center font-semibold">Sign Up</Text>
-                </View>
+                </Pressable>
 
                 <View className='w-[95%] flex justify-center items-center mt-8'>
                     <Text className='font-body text-md font-normal text-gray-600'>
                         Already have an account?
-                        <Text className='font-bold'> Sign In</Text>
+                        <Link href='./login'>
+                            <Text className='font-bold'> Sign In</Text>
+                        </Link>
                     </Text>
                 </View>
             </View>
